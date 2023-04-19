52e92df35c1b068f266c91d304dd78b7a0e68770
remmachezoubir <remmachezoubir7@gmail.com>
2023-04-19 14:47:12 +0100

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
   const [loading, setLoading] = useState(false)
   const handleInput = (e: React.MouseEvent) => {
     e.preventDefault()
+    if (input) {
     setOutput(null)
     handleRequest(input)
     setInput('')
-  //   if (input) {
-  // }else{
+  }else{
 
-  //     setOutput("please enter a text ")
-  //   }
+      setOutput("please enter a text ")
+    }
   }
 
   const handleRequest= async (input:any)=>{
@@ -41,15 +41,16 @@ function App() {
   }
 
   return (
-    <div className='flex justify-center xl:px-40 pt-1 px-4'>
-      <div className='w-full  bg-slate-400 text-lg font-semibold lg:h-[32rem] h-[28rem]  overflow-y-scroll rounded-lg shadow-md shadow-violet-800  md:p-4 p-1'>
+    <div className=' bg-black w-screen text-white  h-screen p-2 overflow-hidden flex flex-col'>
+      <h1 className='text-white self-center  shadow-indigo-500 text-xl font-bold border-b-4 rounded-md'>BASSEM</h1>
+      <div className=' h-full px-4 p-1 mt-4 overflow-scroll border-2 rounded-lg text-lg font-semibold  bg-black'>
         {output? output :loading ? <Loading /> : "enter new text ... "  } 
       </div>
-      <div className='fixed flex sm:flex-nowrap flex-wrap justify-around md:gap-3 bottom-4  w-full xl:px-40' >
-        <textarea value={input} name={"text"} cols={30} onChange={(e) => setInput(e.target.value)} rows={5} className=' shadow-md  shadow-violet-800 p-2 outline-none border-4 focus:border-indigo-700 sm:w-full w-64 bg-slate-300 resize-none rounded-md  '>
+      <div className=' h-48 p-2 rounded-lg flex gap-2 ' >
+        <textarea   value={input} name={"text"} cols={30} onChange={(e) => setInput(e.target.value)} rows={5} className=' font-semibold focus:border-indigo-600 focus:border-2 text-black rounded-xl p-2 text-lg w-72'>
 
         </textarea>
-        <button onClick={(e) => handleInput(e)}><FaTelegramPlane size={40} /></button>
+        <button onClick={(e) => handleInput(e)}><FaTelegramPlane className=' fill-white' size={40} /></button>
       </div>
     </div>
   )
